a9ed1545c3da03e6bf9eab2c56c5b05a5f679d6b
Bahar <abdullohbahar@gmail.com>
2024-09-19 05:11:58 +0700

    persiapan notif email

--- app/Http/Controllers/Pemohon/DashboardPemohonController.php
+++ app/Http/Controllers/Pemohon/DashboardPemohonController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Pemohon;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Notifications\EmailNotification;
 use Illuminate\Http\Request;
 
 class DashboardPemohonController extends Controller
@@ -10,5 +12,14 @@ class DashboardPemohonController extends Controller
     public function index()
     {
         return view('pemohon.dashboard.index');
+    }
+
+    public function tesEmail()
+    {
+        $id = auth()->user()->id;
+        $user = User::findorfail($id);
+
+        $user->notify(new EmailNotification());
+        dd($user);
     }
 }
